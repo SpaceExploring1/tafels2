848ab52700507f2da33f25a524a3e011edecb605
SpaceExploring1 <40209752@yonder.nl>
2025-06-25 12:44:24 +0200

    tafel tabel weggehaald+env key gemaakt+tafelcontroller aangemaakt

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TafelController;  // ← Import the controller here
+
+Route::resource('tafels', TafelController::class);
 
 Route::get('/', function () {
     return view('welcome');
